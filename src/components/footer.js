d27b811b8f7211a5fe0ffd2c0cd4bac70fd86c19
Sushant Bansal <sushantbansal2@gmail.com>
2020-07-04 02:23:42 -0500

    Separated Data from footer to data dir

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,29 +1,33 @@
 import React from "react"
 import { Paper, IconButton } from "@material-ui/core"
 import { Twitter, LinkedIn, Email, GitHub } from "@material-ui/icons"
+import LinkUrls from "../data/FooterList"
 
 const Footer = () => {
+
+  const {email, github, linkedin, twitter} = LinkUrls
+  
   return (
     <Paper className="footer" elevation={2} color="default">
       <div className="footerContent">
         <div>
-          <IconButton href="mailto:" aria-label="Email Me">
+          <IconButton href={email} aria-label="Email Me">
             <Email fontSize="large" color="secondary" />
           </IconButton>
           <IconButton
-            href="https://www.github.com"
+            href={github}
             aria-label="Github Profile"
           >
             <GitHub fontSize="large" color="secondary" />
           </IconButton>
           <IconButton
-            href="https://www.linkedin.com/"
+            href={linkedin}
             aria-label="LinkedIn Profile"
           >
             <LinkedIn fontSize="large" color="secondary" />
           </IconButton>
           <IconButton
-            href="https://www.twitter.com/"
+            href={twitter}
             aria-label="Twitter Link"
           >
             <Twitter fontSize="large" color="secondary" />
---
+++ src/data/FooterList.js
@@ -0,0 +1,8 @@
+const LinkUrls = {
+    email:"mailto:",
+    github:"https://www.github.com",
+    linkedin:"https://www.linkedin.com/",
+    twitter:"https://www.twitter.com/"
+}
+
+export default LinkUrls
