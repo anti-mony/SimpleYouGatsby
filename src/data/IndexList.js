96d849d2024c7829795953b99a5bf3a405aab28f
Sushant Bansal <sushantbansal2@gmail.com>
2021-06-11 00:49:57 -0500

    Added new more option to index page

--- src/data/IndexList.js
+++ src/data/IndexList.js
@@ -5,5 +5,6 @@ export const IndexList = {
         "last": "Doe"
     },
     "title":"I'm a goofball working at Goofball Inc.,  Goofballiapolis, Jupyter",
-    "subtitle": "I like to not goof around in my free time, because that's my real job. Really at Jupyter."
+    "subtitle": "I like to not goof around in my free time, because that's my real job. Really at Jupyter.",
+    "more":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor cursus quam, eu gravida mauris lacinia eget. Sed sed dolor sed neque volutpat imperdiet. Aenean dapibus aliquet tempor. Aliquam a ornare risus. Morbi blandit orci eu arcu tristique, vel imperdiet nunc tempus. Nam elit dui, gravida euismod varius in, malesuada eu erat. Donec gravida placerat ipsum. Mauris porttitor sollicitudin lorem, et iaculis eros placerat ac. Suspendisse potenti. Vivamus sit amet libero sit amet ligula ultricies malesuada.",
 }
