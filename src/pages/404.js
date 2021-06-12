d2946eb8a91cd4f4ff8e4349b74d11a8327549f2
Sushant Bansal <sushantbansal2@gmail.com>
2021-06-12 18:41:33 -0500

    Removed unused component, updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,13 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import { StaticImage } from "gatsby-plugin-image"
 import { Typography } from "@material-ui/core"
 
 const NotFoundPage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      file(relativePath: { eq: "404doggo.webp" }) {
-        childImageSharp {
-          fluid(maxWidth: 1920) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
-  console.log(data)
+
   return (
     <Layout>
       <Seo title="404: Not found" />
@@ -27,15 +16,17 @@ const NotFoundPage = () => {
       </Typography>
       <Typography variant="subtitle1">
         <div className="page-subtitle">
-          You just hit a route that doesn&#39;t exist... the sadness. But this
-          might cheer you up !
+          You just hit a url that doesn&#39;t exist... big oof. But this
+          might help a bit !
         </div>
       </Typography>
       <div className="index-content">
-        {/* <Img
-          fluid={data.file.childImageSharp.fluid}
-          className="fluid-image"
-        ></Img> */}
+      <StaticImage
+            src="../images/404doggo.webp"
+            alt="Beautiful golder retriever holding a tulip in it's mouth."
+            placeholder="Beautiful golder retriever holding a tulip in it's mouth."
+            className="fluid-image"
+          />
         <a
           className="image-credits"
           href="https://unsplash.com/@richardbrutyo?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
